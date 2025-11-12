d101db9bd41041e821ffdb6fe63fbc681cba38b8
Simon Mulligan <95179669+Mully88@users.noreply.github.com>
2025-11-12 12:05:52 +0000

    Create Activity 2 - Python Basics.py
    User enters 2 integers and the first is divided by the second number

---
+++ Activity 2 - Python Basics.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter an Integer: "))        #usee enters a number
+num2 = int(input("Enter another Integer: "))  #user enters another number
+
+sum_result = num1 // num2        #calculates the division of two numbers
+print("The first number divided by the second is:", sum_result) #prints the division of the two numbers
